6606b2329226bdc0aa920442d2ff8fbea58c909d
Core95L <128723248+Core95L@users.noreply.github.com>
2023-10-19 11:42:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ function TocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-const liastDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
 
+//enquanto
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[0].onclick = TocaSomPom;
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
