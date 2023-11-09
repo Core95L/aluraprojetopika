da1050f6c9c4f1e83454bbe93995e052c17823a4
Core95L <128723248+Core95L@users.noreply.github.com>
2023-11-09 11:42:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,60 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
 
-function TocaSomPom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-//enquanto
-while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-    listaDeTeclas[contador].onclick = function () {
-        TocaSomPom('#som_tecla_pom');
+    tecla.onclick = function () {
+        tocaSom(idAudio);
     }
 
-    contador = contador + 1;
+    tecla.onkeydown = function (evento) {
 
-    console.log(contador);
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {function tocaSom (seletorAudio) {
+        const elemento = document.querySelector(seletorAudio);
+    
+        if (elemento && elemento.localName === 'audio') {
+            elemento.play();
+        }
+        else {
+            //alert('Elemento não encontrado');
+            console.log('Elemento não encontrado ou seletor inválido');
+        }
+    
+    }
+    
+    const listaDeTeclas = document.querySelectorAll('.tecla');
+    
+    //para
+    for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    
+        const tecla = listaDeTeclas[contador];
+        const instrumento = tecla.classList[1];
+        const idAudio = `#som_${instrumento}`; //template string
+    
+        tecla.classList.remove('ativa');
+    }
 
 }
